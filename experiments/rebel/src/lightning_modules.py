fix(benchmarker): fix error in precision computation

experiments/rebel/src/lightning_modules.py
@@ -1,6 +1,7 @@
 from typing import Any
 import nltk
 import json
+import json
 import pytorch_lightning as pl
 import torch
 torch.cuda.empty_cache()
@@ -97,7 +98,12 @@ relations_wikidata_movies = ["record label", "professional field", "death date",
 ]
 """
 
-relations_wikidata_movies = ["director", "screenwriter", "genre", "based on", "cast member", "award received", "production company", "country of origin", "publication date", "characters", "narrative location", "filming location", "main subject", "nominated for", "cost"]
+relations_wikidata_movies = [
+    "director", "screenwriter", "genre", "based on", "cast member", "award received", 
+    "production company", "country of origin", "publication date", "characters", 
+    "narrative location", "filming location", "main subject", "nominated for", "cost"
+]
+
 relations_wikidata_movie_schemas = [
     {"pid": "P57", "label": "director", "domain": "film","range": "human"},
 	{"pid": "P58", "label": "screenwriter", "domain": "film", "range": "human"},
@@ -261,10 +267,15 @@ class BaseLightningModule(pl.LightningModule):
             res = []
             for a in triples_per_beam:
                 res += a
-                
+            
+            # TODO : see if list(set(res)) impacts precision, it might depending on the score computation logic.
+            dset = set()
+            for dic in res:
+                dset.add(json.dumps(dic, sort_keys=True))
+            
+            res = [json.loads(dic_str) for dic_str in dset]
             triplets: np.array[dict] = np.array(res)
             
-                
             if self.hparams.relation_mapping:
                 triple_embeddings: Tensor = self.sent_embedder.encode( [triple["head"] + " " + triple["type"] + " " + triple["tail"] for triple in triplets] )
 
@@ -502,7 +513,7 @@ class BaseLightningModule(pl.LightningModule):
                                                          list(relations_nyt.values()), "strict")
             
             
-            elif self.hparams.dataset_name.split('/')[-1] == 'text2kgbench.py':
+            elif self.hparams.dataset_name.split('/')[-1] == 'wikidata_synthetic.py':
                 scores, precision, recall, f1 = re_score([item for pred in self.val_predictions for item in pred['predictions']], 
                                                          [item for pred in self.val_predictions for item in pred['labels']], 
                                                          relations_wikidata_movies)
@@ -551,7 +562,7 @@ class BaseLightningModule(pl.LightningModule):
             elif self.hparams.dataset_name.split('/')[-1] == 'docred_typed.py':
                 relations_docred = {"P6": "head of government", "P17": "country", "P19": "place of birth", "P20": "place of death", "P22": "father", "P25": "mother", "P26": "spouse", "P27": "country of citizenship", "P30": "continent", "P31": "instance of", "P35": "head of state", "P36": "capital", "P37": "official language", "P39": "position held", "P40": "child", "P50": "author", "P54": "member of sports team", "P57": "director", "P58": "screenwriter", "P69": "educated at", "P86": "composer", "P102": "member of political party", "P108": "employer", "P112": "founded by", "P118": "league", "P123": "publisher", "P127": "owned by", "P131": "located in the administrative territorial entity", "P136": "genre", "P137": "operator", "P140": "religion", "P150": "contains administrative territorial entity", "P155": "follows", "P156": "followed by", "P159": "headquarters location", "P161": "cast member", "P162": "producer", "P166": "award received", "P170": "creator", "P171": "parent taxon", "P172": "ethnic group", "P175": "performer", "P176": "manufacturer", "P178": "developer", "P179": "series", "P190": "sister city", "P194": "legislative body", "P205": "basin country", "P206": "located in or next to body of water", "P241": "military branch", "P264": "record label", "P272": "production company", "P276": "location", "P279": "subclass of", "P355": "subsidiary", "P361": "part of", "P364": "original language of work", "P400": "platform", "P403": "mouth of the watercourse", "P449": "original network", "P463": "member of", "P488": "chairperson", "P495": "country of origin", "P527": "has part", "P551": "residence", "P569": "date of birth", "P570": "date of death", "P571": "inception", "P576": "dissolved, abolished or demolished", "P577": "publication date", "P580": "start time", "P582": "end time", "P585": "point in time", "P607": "conflict", "P674": "characters", "P676": "lyrics by", "P706": "located on terrain feature", "P710": "participant", "P737": "influenced by", "P740": "location of formation", "P749": "parent organization", "P800": "notable work", "P807": "separated from", "P840": "narrative location", "P937": "work location", "P1001": "applies to jurisdiction", "P1056": "product or material produced", "P1198": "unemployment rate", "P1336": "territory claimed by", "P1344": "participant of", "P1365": "replaces", "P1366": "replaced by", "P1376": "capital of", "P1412": "languages spoken, written or signed", "P1441": "present in work", "P3373": "sibling"}
                 scores, precision, recall, f1 = re_score([item for pred in self.test_predictions for item in pred['predictions']], [item for pred in self.test_predictions for item in pred['labels']], list(relations_docred.values()), "strict")            
-            elif self.hparams.dataset_name.split('/')[-1] == 'text2kgbench.py':
+            elif self.hparams.dataset_name.split('/')[-1] == 'wikidata_synthetic.py':
                 scores, precision, recall, f1 = re_score([item for pred in self.test_predictions for item in pred['predictions']], [item for pred in self.test_predictions for item in pred['labels']], relations_wikidata_movies)
             else:
                 scores, precision, recall, f1 = re_score([item for pred in self.test_predictions for item in pred['predictions']], [item for pred in self.test_predictions for item in pred['labels']], ['killed by', 'residence', 'location', 'headquarters location', 'employer'])