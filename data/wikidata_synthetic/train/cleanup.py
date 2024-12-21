375ea5d4bff087856ea94aefdca057ad827488e9
Freeman Arthur <swissarthurfreeman@gmail.com>
2024-12-21 16:58:10 +0100

    data(benchmarker): done removing duplicates

--- data/wikidata_synthetic/train/cleanup.py
+++ data/wikidata_synthetic/train/cleanup.py
@@ -1,18 +1,21 @@
 import json
 
-with open("ont_2_music_val.jsonl", "r") as f:
+onto_name = "ont_10_culture"
+data_split = "train"
+
+with open(f"{onto_name}_{data_split}.jsonl", "r") as f:
     data = [json.loads(line) for line in f]
     
     res = {}
     for line in data:
         res[line['triples'][0]['sqid']] = {
-            'id': f'ont_2_music_train_{line["triples"][0]["sqid"]}',
+            'id': f'{onto_name}_{data_split}_{line["triples"][0]["sqid"]}',
             'sent': line['sent'],
             'triples': line['triples']   
         }
         
     for value in res.values():
-        with open("ont_2_music_train_clean.jsonl", "a") as g:
+        with open(f"{onto_name}_{data_split}_clean.jsonl", "a") as g:
             g.write(json.dumps(value) + "\n")
         
         
