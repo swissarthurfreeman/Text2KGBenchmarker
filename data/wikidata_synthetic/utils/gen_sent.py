e628b6bfba1e6e62a6a03c18521d3c84a67a02a0
swissarthurfreeman <swissarthurfreeman@gmail.com>
2025-01-03 20:34:26 +0100

    refactor(benchmarker): make gen_sent presentable, provide examples

--- data/wikidata_synthetic/utils/gen_sent.py
+++ data/wikidata_synthetic/utils/gen_sent.py
@@ -13,12 +13,12 @@ class SentenceGenerator:
     """
     >>> samples = [
             {"triples": [
-                {"sub": "Violent Silences", "sqid": "Q7933178", "rel": "publication date", "rpid": "P577", ..., "oqid": "2004-01-01T00:00:00Z"}, 
+                {"sub": "Violent Silences", "sqid": "Q7933178", "rel": "publication date", "rpid": "P577", "obj": "2004-01-01T00:00:00Z", "oqid": "2004-01-01T00:00:00Z"}, 
                 {"sub": "Violent Silences", "sqid": "Q7933178", "rel": "performer", "rpid": "P175", "obj": "Rico", "oqid": "Q7332245"}
             ]},
             {"triples": [
                 {"sub": "Get Some Sleep", "sqid": "Q5554174", "rel": "part of", "rpid": "P361", "obj": "Beautiful Collision", "oqid": "Q4877686"}, 
-                {"sub": "Beautiful Collision", "sqid": "Q4877686", "rel": "publication date", "rpid": "P577", ..., "oqid": "2002-01-01T00:00:00Z"}, 
+                {"sub": "Beautiful Collision", "sqid": "Q4877686", "rel": "publication date", "rpid": "P577", "obj": "2002-01-01T00:00:00Z", "oqid": "2002-01-01T00:00:00Z"},
                 {"sub": "Beautiful Collision", "sqid": "Q4877686", "rel": "genre", "rpid": "P136", "obj": "pop music", "oqid": "Q37073"}, 
                 {"sub": "Beautiful Collision", "sqid": "Q4877686", "rel": "performer", "rpid": "P175", "obj": "Bic Runga", "oqid": "Q467035"}
             ]}
@@ -64,6 +64,8 @@ class SentenceGenerator:
             print(f"T{i} Getting prompt and triples for", sample['id'])
             prompt, chosen_triples = self.getPrompt(sample['triples'])
             
+            print(prompt)
+            exit(0)
             chat_completion = client.chat.completions.create(
                 model="gpt-4o-mini",
                 messages=[{"role": "user", "content": prompt}],
@@ -76,19 +78,18 @@ class SentenceGenerator:
                 print(json.dumps(res))
                 continue
             
-            with open(output_file_path, "a", encoding='utf-8') as f:
+            with open(self.output_file_path, "a", encoding='utf-8') as f:
                 f.write(json.dumps(res) + "\n")
 
     def getPrompt(self, triples: list[dict]) -> tuple[str, list[dict]]:
         prompt = f"""Your task is to generate varied, accurate, sentences about {self.prompt_subject} from a list of triples following these precise instructions :
-        1) Make the sentences natural, as they would appear in text or conversation, wikipedia, news or the internet.
-        2) The triples can be explicit in the sentence or implied, include all the triples provided, do not invent additional facts. 
-        3) If no currency or timezone are specified, do not include them.
-        4) Do not put names or titles between quotation marks like " or ' unless absolutely necessary.
-        5) Use varied turn of phrases, don't always start the sentence with the name of the main subject, vary using coreference instead of repeating the title. 
-        6) Vary the output size, sometimes a single sentence, sometimes a small paragraph, not more than a couple of sentences.
-        Triples:\n
-        """
+1) Make the sentences natural, as they would appear in text or conversation, wikipedia, news or the internet.
+2) The triples can be explicit in the sentence or implied, include all the triples provided, do not invent additional facts. 
+3) If no currency or timezone are specified, do not include them.
+4) Do not put names or titles between quotation marks like " or ' unless absolutely necessary.
+5) Use varied turn of phrases, don't always start the sentence with the name of the main subject, vary using coreference instead of repeating the title. 
+6) Vary the output size, sometimes a single sentence, sometimes a small paragraph, not more than a couple of sentences.
+Triples:\n"""
         triples = self.getRandomNTriplesFrom(triples)
         prompt += self.getTextTriplesList(triples)
         return prompt, triples
@@ -103,15 +104,17 @@ class SentenceGenerator:
         """Here we do custom random BFS like graph exploration from the first triple to generate a 
         list of n connected triples. No need for node labeling as graph has no cycles."""
         res: list[dict] = []
-        queue: list[dict] = [triples[0]]
+        queue: list[dict] = []
         
-        curr = queue.pop(0)
+        curr = triples[0] 
         res.append(curr)
+        queue.append(curr)
         
-        n_triples = random.randint(3, max_n)
+        n_triples = random.randint(2, max_n)
+        print("n_triples", n_triples)
         if len(triples) < n_triples: return triples     # more triples requested than available, return all triples
         
-        while len(res) != n_triples and len(queue) > 0:
+        while len(res) != n_triples:
             queue += [triple for triple in triples if ( triple['sqid'] == curr['sqid'] or curr['oqid'] == triple['sqid']) and triple not in res]
             random.shuffle(queue)
             curr = queue.pop(0)
@@ -143,11 +146,12 @@ def worker(i, samples, output_file_path, prompt_subject, ont_name):
     gen.generate(stdout=False)
 
 if __name__ == "__main__":
-    ontology_name    = sys.argv[1]  # 'ont_2_music'
-    prompt_subject   = sys.argv[2]  # 'musical works'
-    raw_triples_path = sys.argv[3]  # './raw_triples/ont_2_music_triples.jsonl 
-    output_file_path = sys.argv[4]  # './some_path/ont_2_music_train.jsonl'
     
+    ontology_name    = sys.argv[1]      # 'ont_2_music'
+    prompt_subject   = sys.argv[2]      # 'musical works'
+    raw_triples_path = sys.argv[3]      # './raw_triples/ont_2_music_triples.jsonl 
+    output_file_path = sys.argv[4]      # './some_path/ont_2_music_train.jsonl'
+
     samples = getSamplesWithoutSentencesInOutputFile(raw_triples_path, output_file_path)
 
     print("Found", len(samples), "that haven't been verbalized...")
@@ -160,3 +164,59 @@ if __name__ == "__main__":
     
     pool.close()
     pool.join()
+    """
+    lotr_sample = {
+        "triples": [
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "director", "rpid": "P57", "obj": "Peter Jackson", "oqid": "Q4465"}, 
+            {"sub": "Peter Jackson", "sqid": "Q4465", "rel": "country of citizenship", "rpid": "P27", "obj": "New Zealand", "oqid": "Q664"}, 
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "screenwriter", "rpid": "P58", "obj": "Peter Jackson", "oqid": "Q4465"}, 
+            {"sub": "Peter Jackson", "sqid": "Q4465", "rel": "country of citizenship", "rpid": "P27", "obj": "New Zealand", "oqid": "Q664"}, 
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "screenwriter", "rpid": "P58", "obj": "Philippa Boyens", "oqid": "Q116854"}, 
+            {"sub": "Philippa Boyens", "sqid": "Q116854", "rel": "country of citizenship", "rpid": "P27", "obj": "New Zealand", "oqid": "Q664"}, 
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "screenwriter", "rpid": "P58", "obj": "Fran Walsh", "oqid": "Q116861"}, 
+            {"sub": "Fran Walsh", "sqid": "Q116861", "rel": "country of citizenship", "rpid": "P27", "obj": "New Zealand", "oqid": "Q664"}, 
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "screenwriter", "rpid": "P58", "obj": "Stephen Sinclair", "oqid": "Q3498652"}, 
+            {"sub": "Stephen Sinclair", "sqid": "Q3498652", "rel": "country of citizenship", "rpid": "P27", "obj": "New Zealand", "oqid": "Q664"}, 
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "genre", "rpid": "P136", "obj": "drama film", "oqid": "Q130232"}, 
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "genre", "rpid": "P136", "obj": "fantasy film", "oqid": "Q157394"}, 
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "genre", "rpid": "P136", "obj": "action film", "oqid": "Q188473"}, 
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "genre", "rpid": "P136", "obj": "film based on a novel", "oqid": "Q52207399"}, 
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "based on", "rpid": "P144", "obj": "The Two Towers", "oqid": "Q332388"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "cast member", "rpid": "P161", "obj": "Peter Jackson", "oqid": "Q4465"},
+            {"sub": "Peter Jackson", "sqid": "Q4465", "rel": "country of citizenship", "rpid": "P27", "obj": "New Zealand", "oqid": "Q664"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "cast member", "rpid": "P161", "obj": "John Rhys-Davies", "oqid": "Q16455"},
+            {"sub": "John Rhys-Davies", "sqid": "Q16455", "rel": "country of citizenship", "rpid": "P27", "obj": "United Kingdom", "oqid": "Q145"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "cast member", "rpid": "P161", "obj": "Hugo Weaving", "oqid": "Q42204"},
+            {"sub": "Hugo Weaving", "sqid": "Q42204", "rel": "country of citizenship", "rpid": "P27", "obj": "United Kingdom", "oqid": "Q145"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "cast member", "rpid": "P161", "obj": "Orlando Bloom", "oqid": "Q44467"},
+            {"sub": "Orlando Bloom", "sqid": "Q44467", "rel": "country of citizenship", "rpid": "P27", "obj": "United Kingdom", "oqid": "Q145"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "cast member", "rpid": "P161", "obj": "Cate Blanchett", "oqid": "Q80966"},
+            {"sub": "Cate Blanchett", "sqid": "Q80966", "rel": "country of citizenship", "rpid": "P27", "obj": "United States of America", "oqid": "Q30"},
+            {"sub": "Cate Blanchett", "sqid": "Q80966", "rel": "country of citizenship", "rpid": "P27", "obj": "Australia", "oqid": "Q408"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "award received", "rpid": "P166", "obj": "Academy Award for Best Visual Effects", "oqid": "Q393686"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "award received", "rpid": "P166", "obj": "Academy Award for Best Sound Editing", "oqid": "Q488645"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "award received", "rpid": "P166", "obj": "Hugo Award for Best Dramatic Presentation, Long Form", "oqid": "Q1056240"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "production company", "rpid": "P272", "obj": "New Line Cinema", "oqid": "Q79202"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "production company", "rpid": "P272", "obj": "WingNut Films", "oqid": "Q3569323"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "country of origin", "rpid": "P495", "obj": "United States of America", "oqid": "Q30"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "country of origin", "rpid": "P495", "obj": "New Zealand", "oqid": "Q664"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "publication date", "rpid": "P577", "obj": "2002-12-05T00:00:00Z", "oqid": "2002-12-05T00:00:00Z"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "publication date", "rpid": "P577", "obj": "2002-12-18T00:00:00Z", "oqid": "2002-12-18T00:00:00Z"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "publication date", "rpid": "P577", "obj": "2003-01-16T00:00:00Z", "oqid": "2003-01-16T00:00:00Z"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "characters", "rpid": "P674", "obj": "Gollum", "oqid": "Q15007"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "characters", "rpid": "P674", "obj": "Frodo Baggins", "oqid": "Q177329"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "characters", "rpid": "P674", "obj": "Gandalf", "oqid": "Q177499"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "characters", "rpid": "P674", "obj": "Aragorn", "oqid": "Q180322"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "characters", "rpid": "P674", "obj": "Galadriel", "oqid": "Q204274"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "nominated for", "rpid": "P1411", "obj": "Academy Award for Best Picture", "oqid": "Q102427"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "nominated for", "rpid": "P1411", "obj": "Academy Award for Best Production Design", "oqid": "Q277751"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "nominated for", "rpid": "P1411", "obj": "Academy Award for Best Film Editing", "oqid": "Q281939"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "nominated for", "rpid": "P1411", "obj": "Academy Award for Best Visual Effects", "oqid": "Q393686"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "nominated for", "rpid": "P1411", "obj": "Academy Award for Best Sound", "oqid": "Q830079"},
+            {"sub": "The Lord of the Rings: The Two Towers", "sqid": "Q164963", "rel": "cost", "rpid": "P2130", "obj": "94000000", "oqid": "94000000"}
+        ]
+    }
+    
+    gen = SentenceGenerator([lotr_sample], '', 'films', 'ont_1_movie')
+    gen.generate(stdout=True)
+    """
